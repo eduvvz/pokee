c5ab4334b538a73e0391ff1e6c188464c48db920
= <=>
2018-07-15 20:44:12 -0300

    adicionando header e o menu

---
+++ src/components/shared/header/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <b-container fluid class="header center-inside">
+        <b-row>
+            <h1>Poké<span>Shop</span></h1>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '../../../assets/css/fonts.scss';
+
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+    background-image: url('../../../assets/pokemon.jpg');
+    background-repeat: no-repeat;
+    background-position-x: center;
+    height: 150px;
+    font-family: 'pokemon';
+    background-color: black;
+
+    h1 {
+        color: white;
+        span{
+            color: red;
+        }
+    }
+
+    @media screen and (max-width: 768px){
+        background-image: url('../../../assets/pokemon-mobile.jpg');
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+}
+</style>
+
+
---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+<b-navbar toggleable="md">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">
+    <img src="../../../assets/pokeball.png" alt="">
+  </b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+    <b-navbar-nav class="ml-auto">
+      <b-navbar-nav>
+        <b-nav-item href="#" class="link">Home</b-nav-item>
+        <b-nav-item href="#" class="link">Buscar Pokémon</b-nav-item>
+        <b-nav-item href="#" class="link">Comparar</b-nav-item>
+      </b-navbar-nav>   
+    </b-navbar-nav>
+  </b-collapse>
+</b-navbar>    
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+export default {};
+</script>
+
+<style lang="scss" scoped>
+
+.link {
+  color: black;
+}
+
+</style>
